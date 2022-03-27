cbdf93713da082db35371f26643e0e52eb761da0
Gede Sudimahendra <54178249+deedima3@users.noreply.github.com>
2022-03-27 17:03:10 +0800

    [ enhance ] Add content in explore
    Add new component landscapecard and category button

---
+++ components/Card/CategoryButton.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Button, Text, StyleSheet, useColorScheme, View, Pressable } from 'react-native'
+import Colors from '../../constants/Colors';
+
+const CategoryButton = ({category, navigate} : CategoryButtonProps) => {
+    const onClick = () => {}
+
+  return (
+    <Pressable onPress={onClick} style={styles.pressable}>
+        <Text style={styles.text}>{category}</Text>
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+    pressable: {
+        borderColor : Colors["dark"].tint,
+        borderWidth : 2, 
+        borderRadius : 15,
+        maxHeight : 40,
+        padding : 7,
+        marginLeft : 10
+    },
+    text: {
+        color : Colors["dark"].tint,
+        fontSize : 12,
+    }
+})
+
+export interface CategoryButtonProps{
+    navigate : string;
+    category : string;
+}
+
+export default CategoryButton
